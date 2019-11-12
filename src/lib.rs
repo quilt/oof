92cefffa55031aa943dd0a469bd84792fbf8928a
Matt Garnett <matt.garnett@me.com>
2019-11-12 15:54:33 -0700

    add root calculation

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 #![cfg_attr(not(test), no_std)]
 
+use arrayref::array_ref;
+use bonsai::expand;
+use sha2::{Digest, Sha256};
+
 type K = u128;
 type V = u128;
 
@@ -7,11 +11,12 @@ pub struct Proof<'a> {
     pub keys: &'a [K],
     pub values: &'a mut [V],
     pub height: u32,
+    is_dirty: bool,
 }
 
 #[cfg_attr(test, derive(Debug, PartialEq))]
 pub enum Error {
-    EntryNotFound,
+    EntryNotFound(K),
 }
 
 impl<'a> Proof<'a> {
@@ -20,6 +25,7 @@ impl<'a> Proof<'a> {
             keys,
             values,
             height,
+            is_dirty: false,
         }
     }
 
@@ -35,11 +41,49 @@ impl<'a> Proof<'a> {
             Ok(index) => {
                 let old = self.values[index];
                 self.values[index] = value;
+                self.is_dirty = true;
                 Ok(old)
             }
-            Err(_) => Err(Error::EntryNotFound),
+            Err(_) => Err(Error::EntryNotFound(key)),
         }
     }
+
+    pub fn root(&mut self) -> Result<&V, Error> {
+        if self.is_dirty {
+            self.refresh()?;
+        }
+
+        Ok(self.get(&1).ok_or(Error::EntryNotFound(1))?)
+    }
+
+    fn refresh(&mut self) -> Result<(), Error> {
+        let mut position = self.keys.len() - 1;
+
+        while position > 0 {
+            let (left, right, parent) = expand(self.keys[position]);
+
+            let left = self.get(&left).ok_or(Error::EntryNotFound(left))?;
+            let right = self.get(&right).ok_or(Error::EntryNotFound(right))?;
+
+            let mut buf = [0u8; 32];
+            hash_children(&mut buf, left, right);
+
+            self.set(parent, u128::from_le_bytes(*array_ref![buf, 0, 16]))?;
+
+            position -= 1;
+        }
+
+        self.is_dirty = false;
+
+        Ok(())
+    }
+}
+
+fn hash_children(buf: &mut [u8; 32], left: &V, right: &V) {
+    buf[0..16].copy_from_slice(&left.to_le_bytes());
+    buf[16..32].copy_from_slice(&right.to_le_bytes());
+    let tmp = Sha256::digest(buf);
+    buf[0..32].copy_from_slice(tmp.as_ref());
 }
 
 #[cfg(test)]
@@ -51,7 +95,8 @@ mod tests {
         let proof = Proof {
             keys: &[1, 2, 3],
             values: &mut [1, 2, 3],
-            height: 2,
+            height: 1,
+            is_dirty: false,
         };
 
         assert_eq!(proof.get(&1), Some(&1));
@@ -65,12 +110,29 @@ mod tests {
         let mut proof = Proof {
             keys: &[1, 2, 3],
             values: &mut [1, 2, 3],
-            height: 2,
+            height: 1,
+            is_dirty: false,
         };
 
         assert_eq!(proof.set(1, 2), Ok(1));
         assert_eq!(proof.set(2, 3), Ok(2));
         assert_eq!(proof.set(3, 4), Ok(3));
-        assert_eq!(proof.set(4, 5), Err(Error::EntryNotFound));
+        assert_eq!(proof.set(4, 5), Err(Error::EntryNotFound(4)));
+    }
+
+    #[test]
+    fn root() {
+        let mut proof = Proof {
+            keys: &[1, 2, 3],
+            values: &mut [1, 2, 3],
+            height: 2,
+            is_dirty: true,
+        };
+
+        let mut buf = [0u8; 32];
+        hash_children(&mut buf, &proof.values[1], &proof.values[2]);
+        let root = u128::from_le_bytes(*array_ref![buf, 0, 16]);
+
+        assert_eq!(proof.root(), Ok(&root));
     }
 }
